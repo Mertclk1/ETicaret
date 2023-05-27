cc94e176397388715f3cb499bc80483a50676522
Hasan Mert Çelik <hmert.clk@gmail.com>
2023-05-27 18:32:52 +0300

    UrunController eklendi

---
+++ ETicaret.UI/Controllers/UrunController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ETicaret.UI.Controllers
+{
+	public class UrunController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
